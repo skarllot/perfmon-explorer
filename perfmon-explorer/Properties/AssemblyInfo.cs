97dc3edd39367fd52408d3045b010aa0df720a0f
Fabrício Godoy <fgodoy@rodrimar.com.br>
2013-07-24 18:01:56 -0300

    Added license headers

--- perfmon-explorer/Properties/AssemblyInfo.cs
+++ perfmon-explorer/Properties/AssemblyInfo.cs
@@ -1,16 +1,37 @@
-﻿using System.Reflection;
+﻿//
+//  AssemblyInfo.cs
+//
+//  Author:
+//       Fabricio Godoy <skarllot@gmail.com>
+//
+//  Copyright (c) 2013 Fabricio Godoy
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("perfmon-analyser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("perfmon-explorer")]
+[assembly: AssemblyDescription("Refer to https://github.com/skarllot/perfmon-explorer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("perfmon-analyser")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("perfmon-explorer")]
+[assembly: AssemblyCopyright("2013 (c) Fabrício Godoy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +53,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
